bad742051c7921838e51ea74a0856293f3310bcb
Ramayanam sai kiran <145695560+saikiranramayanam@users.noreply.github.com>
2023-10-11 12:11:23 +0530

    Add files via upload

---
+++ area_square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	b = a*a;
+	b = 4*a;
+	printf("area of a square with side %d is: %d\n",a,b);
+	printf("perimeter of a square is %d is: %d\n",a,b);
+	return 0;
+}
